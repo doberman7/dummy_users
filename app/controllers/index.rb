1ce485acb9537ec2cdb0a5fa811e073eb634dc0d
codeamac19 <codeamac19@gmail.com>
2017-06-06 20:29:38 -0500

    desarrollando autenticacion

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,39 +1,52 @@
-=begin
-  Iniciar Sesión
-  Cerrar Sesión
-  Crear cuenta de usuario
-  Página secreta
-=end
-def create_usser(u_name,u_email,u_password)
-  Usser.create!(name: u_name,email: u_email,password: u_password)
-end
 
 get '/' do
-  # La siguiente linea hace render de la vista
-  # que esta en app/views/index.erb
   erb :index
 end
+# Crear cuenta de usuario
+get '/sign' do
+  erb :sign_up
+end
+#logearse
+get '/log' do
+  erb :log_in
+end
 
-post '/home' do
+#accion si se escoge el boton de "sign"
+post '/signUP' do
   # entradas del formulario
   u_name = params[:user_name]
   u_email = params[:user_email]
   u_password = params[:user_password]
 
-  user = create_usser(u_name,u_email,u_password)
-
-  if user.save
-    # redirect to("/welcome/#{user.name}")
+  user = Usser.create!(name: u_name,email: u_email,password: u_password)
+  #si se logra salvar el usser
+  if user.save!
+    # Iniciar Sesión
     erb :log_in
+  # en caso de bo salvarse usario ir al index
+  #NO FUNCIONA, las validaaciones en el modelo lo impiden
   else
-    erb :home
+    erb :index
   end
-
 
 end
 
-get '/welcome/:user' do
-  puts "-" * 50
- p params[:user]
+# Página secreta
+post '/user_page' do
+  puts "-" * 100
+  em = params[:email]
+  pass = params[:password]
+
+  @autentication =  Usser.authenticate(em,pass)
+  p @autentication.itself
+
+  if @autentication !=nil
+     erb :secret_page
+  else
+     erb :log_in
+  end
+
+
+
 
 end
--- app/models/usser.rb
+++ app/models/usser.rb
@@ -4,9 +4,11 @@ class Usser < ActiveRecord::Base
 	validates :password, presence: true
 
 
-	#ej.User.authenticate('fernando@codea.mx', 'qwerty')
+
+	# http://api.rubyonrails.org/classes/ActiveRecord/Base.html
 	def self.authenticate(email, password)
-		# si el email y el password corresponden a un usuario valido, regresa el usuario
-		# de otra manera regresa nil
+		#regresar DONDE hay concordancia del input en el formulario(email y password) SI existe un email igual al input en la BD
+		# The exists? method also takes multiple values, but the catch is that it will return true if any one of those records exists.
+		where(email:email , password:password).first if self.exists?(email: email)
 	end
 end
