c0bb60af7a879607becf05e29f41b55f863a5df3
codeamac19 <codeamac19@gmail.com>
2017-06-07 21:25:13 -0500

    developmento of  user.errors.full_messages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,7 @@ require 'sinatra'
 set :show_exceptions, false
 #The exception object can be obtained from the sinatra.error Rack variable:
 error do#.. en lugar de eso se lanza esta opcion
-  @error = "ERROR de validacion"
+  @error = "something's FUCK IT UP"
   erb :sign_up
 end
 #-----------
@@ -26,14 +26,24 @@ end
 #accion si se escoge el boton de "sign"
 post '/signUP' do
   # Asignar a @user entradas del formulario en los PARAMS name, email y password
-  @user = Usser.create!(name: params[:user_name],email: params[:user_email],password: params[:user_password])
+  @user = Usser.new(name: params[:user_name],email: params[:user_email],password: params[:user_password])
+  puts "-"*50
 
+  if @user.errors.any?
+    rong = @user.errors.full_messages
+    p rong
+    p "ssssss"
+  end
+  puts "-"*50
   #si se logra salvar el modelo usser
-  if  @user.save!
+  if @user.save
     #se ve en consola y en layout:
     p session[:saved_message] = "Successfully stored the name: "#en layout se establecio que se visualize el @user.name si este no es nil
-    #renderear pagina de lof in
+    #renderear pagina de log_in
     erb :log_in
+  else
+    @user = @user
+    erb :sign_up
   end
 end
 
