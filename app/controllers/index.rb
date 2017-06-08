808c091dd07db52135181db53bf9fdb092c85bfb
codeamac19 <codeamac19@gmail.com>
2017-06-08 17:02:08 -0500

    development of errors.full_messages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -26,24 +26,17 @@ end
 #accion si se escoge el boton de "sign"
 post '/signUP' do
   # Asignar a @user entradas del formulario en los PARAMS name, email y password
-  @user = Usser.new(name: params[:user_name],email: params[:user_email],password: params[:user_password])
-  puts "-"*50
-
-  if @user.errors.any?
-    rong = @user.errors.full_messages
-    p rong
-    p "ssssss"
-  end
-  puts "-"*50
-  #si se logra salvar el modelo usser
-  if @user.save
-    #se ve en consola y en layout:
-    p session[:saved_message] = "Successfully stored the name: "#en layout se establecio que se visualize el @user.name si este no es nil
-    #renderear pagina de log_in
-    erb :log_in
+@user = Usser.create!(name: params[:user_name],email: params[:user_email],password: params[:user_password])
+#si se logra salvar el modelo usser
+  if  @user.save!
+  #se ve en consola y en layout:
+  p session[:saved_message] = "Successfully stored the name: "#en layout se establecio que se visualize el @user.name si este no es nil
+  #renderear pagina de lof in
+   erb :log_in
+  # en caso de bo sa
   else
-    @user = @user
-    erb :sign_up
+    p "NOOOOOO"
+
   end
 end
 
