f54e021f958ac60de79ae92b9172b8d454d65c20
codeamac19 <codeamac19@gmail.com>
2017-06-08 19:19:28 -0500

    render acceso a pagina secreta FUNCIONAL

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,7 @@ require 'sinatra'
 set :show_exceptions, false
 #The exception object can be obtained from the sinatra.error Rack variable:
 error do#.. en lugar de eso se lanza esta opcion
-  @fuck = "something's FUCK IT UP on      case @user.valid? in       'post '/signUP' "
+  @fuck = "something's FUCK'T UP...some where"
   # erb :sign_up
 end
 #-----------------------------------------------------
@@ -26,7 +26,7 @@ end
 #accion si se escoge el boton de "sign"
 post '/signUP' do
   # Asignar a @user entradas del formulario en los PARAMS name, email y password
-user = Usser.new(name: params[:user_name],email: params[:user_email],password: params[:user_password])
+  user = Usser.new(name: params[:user_name],email: params[:user_email],password: params[:user_password])
 
 
 =begin
@@ -64,7 +64,7 @@ user = Usser.new(name: params[:user_name],email: params[:user_email],password: p
       p user
   end
 
-end
+end#fin de post '/signUP'
 
 # peticion si el login es exitoso
 post '/log_page' do
@@ -72,13 +72,18 @@ post '/log_page' do
   #Autenticar objeto con metodo ".authenticate" creado en MODELO con lo inputs del formulario
   @autentication =  Usser.authenticate(params[:email], params[:password])
   #mostrar inputs en consola
-  puts "-" * 100
-  p @autentication.itself
+
+  puts "AUTENTICATION" + "-" * 100
+  p @autentication
   puts "-" * 100
   #SI la autenticacion no es nil
   if @autentication !=nil
     #ir a pagina secreta
-     erb :secret_page
+
+    p "sesion user_datails" + "-"*100
+    p session[:user_datails] = @autentication    
+    p "-"*100
+    erb :secret_page
   #SINO crear @advert
   else
     #@advert esta enmbebido en log_in.erb
